db1f201bd112d89859c2690fc464ee5cb9e16a84
Kevin dos Reis Hartwig <kevindosreishartwig1221@gmail.com>
2024-04-13 13:18:41 -0300

    criando useState no componente tarefa

--- src/containers/ListaDeTarefas/index.tsx
+++ src/containers/ListaDeTarefas/index.tsx
@@ -1,25 +1,40 @@
 import Tarefa from '../../components/Tarefa'
 import { Container } from './styles'
 
+const tarefas = [
+  {
+    titulo: 'Estudar TypeScript',
+    descricao: 'Ver a aula 3 da EBAC',
+    prioridade: 'Importante',
+    status: 'pendente'
+  },
+  {
+    titulo: 'Pagar a conta de internet',
+    descricao: 'Baixar fatura no Gmail',
+    prioridade: 'urgente',
+    status: 'concluída'
+  },
+  {
+    titulo: 'Ir para a academia',
+    descricao: 'Fazer treino B',
+    prioridade: 'Importante',
+    status: 'pendente'
+  }
+]
 const ListaDeTarefas = () => (
   <Container>
     <p>2 tarefas marcadas como: &quot; categoria &quot; e &quot;termo&quot;</p>
     <ul>
-      <li>
-        <Tarefa />
-      </li>
-      <li>
-        <Tarefa />
-      </li>
-      <li>
-        <Tarefa />
-      </li>
-      <li>
-        <Tarefa />
-      </li>
-      <li>
-        <Tarefa />
-      </li>
+      {tarefas.map((t) => (
+        <li key={t.titulo}>
+          <Tarefa
+            descricao={t.descricao}
+            titulo={t.titulo}
+            status={t.status}
+            prioridade={t.prioridade}
+          />
+        </li>
+      ))}
     </ul>
   </Container>
 )
