ec05648dd6488f3ea6f3be5b152ed36f3851b59f
Kevin dos Reis Hartwig <kevindosreishartwig1221@gmail.com>
2024-04-13 14:57:04 -0300

    usando enums nos em status e prioridade

--- src/containers/ListaDeTarefas/index.tsx
+++ src/containers/ListaDeTarefas/index.tsx
@@ -1,24 +1,25 @@
 import Tarefa from '../../components/Tarefa'
 import { Container } from './styles'
+import * as enums from '../../utils/enums/Tarefa'
 
 const tarefas = [
   {
     titulo: 'Estudar TypeScript',
     descricao: 'Ver a aula 3 da EBAC',
-    prioridade: 'Importante',
-    status: 'pendente'
+    prioridade: enums.Prioridade.IMPORTANTE,
+    status: enums.Status.PENDENTE
   },
   {
     titulo: 'Pagar a conta de internet',
     descricao: 'Baixar fatura no Gmail',
-    prioridade: 'urgente',
-    status: 'concluída'
+    prioridade: enums.Prioridade.URGENTE,
+    status: enums.Status.CONCLUIDA
   },
   {
     titulo: 'Ir para a academia',
     descricao: 'Fazer treino B',
-    prioridade: 'Importante',
-    status: 'pendente'
+    prioridade: enums.Prioridade.IMPORTANTE,
+    status: enums.Status.PENDENTE
   }
 ]
 const ListaDeTarefas = () => (
