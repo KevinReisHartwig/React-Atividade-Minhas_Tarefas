a1b2332caa9063f2087c0a1dbcb5522d91b1cc6b
Kevin dos Reis Hartwig <kevindosreishartwig1221@gmail.com>
2024-04-14 19:38:13 -0300

    filtro de pesquisar tarefas

--- src/containers/ListaDeTarefas/index.tsx
+++ src/containers/ListaDeTarefas/index.tsx
@@ -4,17 +4,25 @@ import { useSelector } from 'react-redux'
 import { RootReducer } from '../../store'
 
 const ListaDeTarefas = () => {
-  const { tarefas } = useSelector((state: RootReducer) => state)
+  const { itens } = useSelector((state: RootReducer) => state.tarefas)
+  const { termo } = useSelector((state: RootReducer) => state.filtro)
+
+  const filtrarTarefa = () => {
+    return itens.filter(
+      (item) => item.titulo.toLowerCase().search(termo.toLowerCase()) >= 0
+    )
+  }
 
   return (
     <Container>
       <p>
-        2 tarefas marcadas como: &quot; categoria &quot; e &quot;termo&quot;
+        2 tarefas marcadas como: &quot; categoria &quot; e &quot;{termo}&quot;
       </p>
       <ul>
-        {tarefas.map((t) => (
+        {filtrarTarefa().map((t) => (
           <li key={t.titulo}>
             <Tarefa
+              id={t.id}
               descricao={t.descricao}
               titulo={t.titulo}
               status={t.status}
